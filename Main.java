c363cf925b5b7b19b27a5bdf89acf650610bb848
Siris St Victor <144554416+MadebySiris@users.noreply.github.com>
2024-11-04 16:18:07 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        AWTExample1 f = new AWTExample1();
+        AWTExample2 awt_obj = new AWTExample2(); 
+        f.setVisible(true);
+        awt_obj.setVisible(true);
+
+        
+    }
+}
